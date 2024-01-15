cca4cace2a9418649934f7fe2566cf617626fe7a
yaseen-yusha <156585225+yaseen-yusha@users.noreply.github.com>
2024-01-15 10:31:28 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,1 +1,24 @@
+/*
+Maintainer: Jayaprakashnarayana.reddy@pearson.com
+version: v0.1
+Description: Function to checkout code from bitbucket
 
+List of Arguments:
+branch: The Branch to clone
+credentialsID: ID of credentials stored in jenkins
+dirPath: The directory where to clone the repository
+url: Complete Bitbucket repository url to clone
+*/
+
+def call(body) {
+    try {
+    dir("${body.dirPath}") {
+    git branch: "${body.branch}", credentialsId: "${body.credentialsID}", url: "${body.url}"
+    }
+    sh " ls -lrt ${b
+    }
+  catch (err) {
+    currentBuild.result = 'FAILED'
+    throw err
+  }
+}
